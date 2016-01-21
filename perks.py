a54a357dfdca0a3eff80b6b3f17ecb0e6da930f0
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-01-21 18:52:58 +0000

    Added __str__ method and tidied up class

--- perks.py
+++ perks.py
@@ -2,15 +2,20 @@
 
 class Perk():
 
-    name = None
-    rank = 0
-    attribute = None
-    attribute_rank = 0
-    character_level = 0
-
     def __init__(self, name, attribute, attribute_rank, rank=1, character_level=0):
         self.name = name
         self.attribute = attribute
         self.attribute_rank = attribute_rank
         self.rank = rank
         self.character_level = character_level
+
+    def __str__(self):
+        return "{name} - Rank {rank} - Requires {attr} at {attr_level}".format(name=self.name, rank=self.rank, attr=self.attribute, attr_level=self.attribute_rank)
+
+    def __eq__(self, other):
+        if not isinstance(other, Perk):
+            return False
+
+        return (self.name == other.name
+            and self.attribute == other.attribute
+            and self.rank == other.rank)
