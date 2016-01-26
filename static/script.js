f6c44dc1a0878f0b968803568c97c0558fb57416
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2016-01-26 21:26:46 +0000

    Added ranks to perks page and js to work with it

--- static/script.js
+++ static/script.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
 
+	$(".perk").bind("contextmenu", function() {
+		var perkname = $(this).find(".perk-name").text()
+		if(perkToRank[perkname] > 0) {
+			perkToRank[perkname]--
+		}
+		console.log("Decrease " + perkname + " - Rank: " + perkToRank[perkname])
+		return false;
+	});
+
 	$(".perk").on("click", function(){
-		console.log("Clicked on " + $(this).text())
+		var perkname = $(this).find(".perk-name").text()
+		var maxRank = 3
+		if(perkToRank[perkname] < maxRank) {
+			perkToRank[perkname]++
+		}
+		console.log("Increase " + perkname + " - Rank: " + perkToRank[perkname])
+	});
+
+	var perkToRank = {}
+
+	$(".perk").each(function(){
+		perkToRank[$(this).find(".perk-name").text()] = 0
 	});
 });
 
